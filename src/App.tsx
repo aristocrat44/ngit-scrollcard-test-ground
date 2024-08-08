c7867b79f36de0cb3319a230cb8ba6d82d5b82ed
aristocrat44 <arcrt1993@gmail.com>
2024-08-08 13:34:15 +0545

    initial commit added scrollcard package

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AnimatedScrollCard } from "animated-scrollcard-ngit";
+import "./App.css";
+import "/node_modules/animated-scrollcard-ngit/dist/index.css";
+
+function App() {
+  return (
+    <div>
+      <AnimatedScrollCard />
+    </div>
+  );
+}
+
+export default App;
